926e224c8374d9e94b4ff63fbca6bf3105ed6fab
borch <borch@3393da94-25e1-425e-878a-45ffd320fc1c>
2007-11-07 19:14:30 +0000

    updated count-lines.pl
    git-svn-id: svn+ssh://login.math.tu-dresden.de/home/st4/borch/repositories/algebra@97 3393da94-25e1-425e-878a-45ffd320fc1c

--- count-lines.pl
+++ count-lines.pl
@@ -5,24 +5,26 @@
 use warnings;
 use strict;
 
-my @filenames = qw/algebras.lisp
-                   basic-algebras.lisp
-                   basic-functions.lisp
-                   equations.lisp
-                   functions-and-relations.lisp
-                   functions.lisp
-                   math-like-notation.lisp
-                   relations.lisp
-                   sets.lisp
-                   signatures.lisp
-                   subalgebras.lisp
-                   tables.lisp
-                   technicals.lisp
-                   terms.lisp
-                   test-cases.lisp
+my @filenames = qw/equations-equations.lisp
+                   functions-and-relations-homomorphisms.lisp
+                   fundamental-functions-basic-functions.lisp
+                   fundamental-functions-functions.lisp
+                   fundamental-functions-relations.lisp
+                   fundamental-functions-tables.lisp
+                   subalgebras-subalgebras.lisp
+                   technicals-math-like-notation.lisp
+                   technicals-sets.lisp
+                   technicals-technicals.lisp
+                   technicals-test-cases.lisp
+                   terms-terms.lisp
                    test-universal-algebra.lisp
+                   uacalc-interface-free-algebra.lisp
+                   uacalc-interface-io.lisp
                    uacalc-interface.lisp
-                   uacalc-technicals.lisp/;
+                   uacalc-interface-uab-standard.lisp
+                   universal-algebra-algebras.lisp
+                   universal-algebra-basic-algebras.lisp
+                   universal-algebra-signatures.lisp/;
 
 my $cmd_string = "wc -l " . join(" ", @filenames);
 print qx/$cmd_string/;

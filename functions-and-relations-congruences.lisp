a637d1873c28ee9820467d1d3cfd12d45c46f3aa
borch <borch@3393da94-25e1-425e-878a-45ffd320fc1c>
2007-11-11 21:39:52 +0000

    first scratch implementation of congruence relations
    git-svn-id: svn+ssh://login.math.tu-dresden.de/home/st4/borch/repositories/algebra@105 3393da94-25e1-425e-878a-45ffd320fc1c

--- functions-and-relations-congruences.lisp
+++ functions-and-relations-congruences.lisp
@@ -1,11 +1,30 @@
 (in-package :functions-and-relations)
 
-(defun relation-is-compatible-with-operation (relation operation algebra)
+(defun relation-is-compatible-with-operation-p (relation operation algebra)
   (declare (type relation relation)
            (type symbol operation)
            (type algebra algebra))
   "Returns non-NIL if RELATION is compatible with OPERATION on ALGEBRA."
   (let ((arity (arity-of-function-symbol (signature-of algebra) operation)))
     (and (not (null arity))
-         (error "Not implemented"))))
-;;; HERE ;;;
+         (forall (tuples (n-elemental-subsets (graph relation) arity))
+           (let ((first-arguments (mapcar #'first tuples))
+                 (second-arguments (mapcar #'second tuples)))
+             (in-relation-p relation
+                            (apply-operation-in-algebra operation first-arguments algebra)
+                            (apply-operation-in-algebra operation second-arguments
+                                                        algebra)))))))
+
+(defun relation-is-compatible-with-all-operations-on-algebra-p (relation algebra)
+  (declare (type relation relation)
+           (type algebra algebra))
+  "Returns non-NIL if RELATION is compatible with all operations on ALGEBRA."
+  (every #'(lambda (op) (relation-is-compatible-with-operation-p relation op algebra))
+         (function-symbols-of (signature-of algebra))))
+
+(defun congruence-relation-p (relation algebra)
+  (declare (type relation relation)
+           (type algebra algebra))
+  "Returns non-NIL if RELATION is a congruence relation on ALGEBRA."
+  (and (equivalence-relation-p relation)
+       (relation-is-compatible-with-all-operations-on-algebra-p relation algebra)))
